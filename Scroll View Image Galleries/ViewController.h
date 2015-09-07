319a0edb1d440b1aa314eb1832c7f36932c79ee6
asu <arsenyk.ustaris.dev@gmail.com>
2015-09-07 14:32:12 -0700

    Initial Commit

---
+++ Scroll View Image Galleries/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Scroll View Image Galleries
+//
+//  Created by asu on 2015-09-07.
+//  Copyright (c) 2015 asu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
